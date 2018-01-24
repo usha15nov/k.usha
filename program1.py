db1ad0e1e60fb0a1b1191ed1223064b51fc5fa1e
usha15nov <35484481+usha15nov@users.noreply.github.com>
2018-01-24 12:44:06 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,7 @@
+# checkthegivennosispositiveornegative
+a=int(input("enter number to check postive or negative"))
+if a>0:
+    print("the given number is positive")
+else:
+    print("the given number is negative")
+   
---
+++ program2.py
@@ -0,0 +1,6 @@
+# checkthenosispostiveornegative
+a=100
+if a>0:
+    print(a,"is a positive number")
+if a<0:
+    print(a,"is a negative number")
